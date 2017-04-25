a9084783756182c37ba10008a0ab2fd372e1793b
da90 <dmitrij1990@hotmail.com>
2017-04-25 21:53:31 +0100

    more css work done

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,12 +3,13 @@ postcss = require("gulp-postcss"),
 autoprefixer = require("autoprefixer"),
 cssvar = require("postcss-simple-vars"),
 cssnest = require("postcss-nested"),
-cssimport = require("postcss-import");
+cssimport = require("postcss-import"),
+mixins = require("postcss-mixins");
 
 gulp.task("styles", function() {
   /*return async function (gulp.src)*/
   return gulp.src("./app/assets/styles/styles.css")
-  .pipe(postcss([cssimport, cssnest, autoprefixer, cssvar]))
+  .pipe(postcss([cssimport, mixins, cssnest, autoprefixer, cssvar]))
   .on("error", function(e) {
     console.log(e.toString());
     this.emit("end");
